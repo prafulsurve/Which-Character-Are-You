b67c6a558291541ccf6a0376f81fc5e66bf4f5a1
Praful Surve <survepraful@live.com>
2017-04-01 12:46:43 -0700

    express

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,18 @@
 (function(window) {
     'use strict';
+    var socket = io.connect('http://localhost:3000');
+    //var SERVER_URL = App.SERVER_URL;
+    //SERVER_URL = 'http://localhost:3002/quiz';
+    //App.SERVER_URL = SERVER_URL;
+    var SERVER_URL  = "";
+    socket.on('setquizid', function(data) {
+        SERVER_URL = data.quizurl;
+        //console.log(remoteServer);
+    });
+
+
     var FORM_SELECTOR = '[data-quiz="form"]';
     var $ = window.jQuery;
-    var SERVER_URL = 'http://localhost:3002/quiz';
     var App = window.App;
     var RemoteServer = App.RemoteServer;
     var remoteServer = new RemoteServer(SERVER_URL);
@@ -19,10 +29,13 @@
         console.log(data);
         $('#myModal .modal-title').text('You are ' + data);
         $('#myModal .modal-body').text('Char Desc');
+<<<<<<< Updated upstream
          //return myTruck.createOrder.call(myTruck, data)
 <<<<<<< HEAD
          $('#myModal').modal('show');
 =======
 >>>>>>> e74922f320d1ff70c0c554f6568b656e565128b4
+=======
+>>>>>>> Stashed changes
       });
 })(window);
